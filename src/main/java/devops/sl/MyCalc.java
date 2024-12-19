fa61b49416448a388ff0c8fe8c5ad4ec55e7263e
consultpavan <158429713+consultpavan@users.noreply.github.com>
2024-12-19 18:01:10 +0530

    Update MyCalc.java
    Test the mvane build-Simplelearn

--- src/main/java/devops/sl/MyCalc.java
+++ src/main/java/devops/sl/MyCalc.java
@@ -27,10 +27,10 @@ public class MyCalc {
 
 		MyCalc calc = new MyCalc();
 		Demo ob = new Demo();
-		System.out.println("Sum is : "+calc.sum(20, 10));
-		System.out.println("Diff is : "+calc.diff(20, 10));
-		System.out.println("Mul is : "+calc.mul(20, 10));
-		System.out.println("Div is : "+calc.div(20, 10));
+		System.out.println("Sum is : "+calc.sum(30, 10));
+		System.out.println("Diff is : "+calc.diff(30, 10));
+		System.out.println("Mul is : "+calc.mul(30, 10));
+		System.out.println("Div is : "+calc.div(30, 10));
 		ob.greet();
 		ob.show();
 	}
